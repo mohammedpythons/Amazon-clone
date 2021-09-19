6092b55d098116290a9a9c0c6d1531423840e616
mohammedpythons <mohammedshokai99@gmail.com>
2021-09-19 18:43:21 -0400

    finish with navBar

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
-import searchIcon from "@mui/icons-material";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 function Header() {
   return (
     <nav className="header">
@@ -15,11 +16,42 @@ function Header() {
       {/* search box */}
       <div className="header__search">
         <input className="header__searchInput" type="text" />
-        <searchIcon className="header__searchIcon" />
+        <SearchIcon className="header__searchIcon" />
       </div>
 
       {/* 3 links */}
+      <div className="header__nav">
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__option__lineOne">Hello Moe</span>
+            <span className="header__option__lineTwo">Sign In</span>
+          </div>
+        </Link>
+
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__option__lineOne">Returns</span>
+            <span className="header__option__lineTwo">& Orders</span>
+          </div>
+        </Link>
+
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__option__lineOne">Your</span>
+            <span className="header__option__lineTwo">prime</span>
+          </div>
+        </Link>
+      </div>
       {/* basket icon  with numbers */}
+
+      <Link to='/checkout' className="header__link">
+        <div className="header__option__Basket">
+          {/* shopping basket icon */}
+          <ShoppingBasketIcon />
+          {/* numbers of items in the basket */}
+          <spam className="header__option__lineTwo header__basketCount">0</spam>
+        </div>
+      </Link>
     </nav>
   );
 }
